3edaa5dab177c2f0e554c7a0254a9fdd79678543
Harald Jörg <haj@posteo.de>
2020-07-16 23:05:52 +0200

    Formalia: Bumped required Emacs-version to 26.3
    The function cperl-time-fontification has been changed last year
    to use time-convert, which has only been released with Emacs 26.3.
    This header keyword is relevant for ELPA/MELPA, but not for the
    version bundled with Emacs.
    
    As far as I see now, the new cperl-mode mostly works with Emacs 26.1.
    I'm interested whether there's more which has been added recently.

--- cperl-mode.el
+++ cperl-mode.el
@@ -8,7 +8,7 @@
 ;; Maintainer: emacs-devel@gnu.org
 ;; Keywords: languages, Perl
 
-;; Package-Requires: ((emacs "26.1"))
+;; Package-Requires: ((emacs "26.3"))
 ;; Package-Version: 7.00
 ;; Homepage: https://github.com/HaraldJoerg/cperl-mode
 
