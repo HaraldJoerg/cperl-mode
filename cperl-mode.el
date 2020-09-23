c6b3612050b90562f80deb4d369e6b139a14dd4a
Harald Jörg <haj@posteo.de>
2020-09-23 18:04:25 +0200

    Remove check for emacs version: This will only apply to the ELPA branch

--- cperl-mode.el
+++ cperl-mode.el
@@ -9,7 +9,6 @@
 ;; Maintainer: emacs-devel@gnu.org
 ;; Keywords: languages, Perl
 
-;; Package-Requires: ((emacs "26.3"))
 ;; Package-Version: 7.00
 ;; Homepage: https://github.com/HaraldJoerg/cperl-mode
 
